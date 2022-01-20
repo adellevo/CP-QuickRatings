d6c598ce56e73ea7210f79cb5357ff427cb700ac
Adelle Vo <adelle.vo@gmail.com>
2022-01-19 19:04:41 -0800

    progress

--- main.js
+++ main.js
@@ -11,15 +11,46 @@ addEval = (document) => {
             color = setTierColor(parseFloat(rating));
             newElement.setAttribute("style", `background-color: ${color}`);
             // two profs
-            if (profArr.length == 2) 
+            if (profArr.length == 2) {
                 newElement.innerHTML = `${profArr[0]} (${rating}), ${profArr[1]} (${rating})`;
+            }
             // only one prof
-            else 
+            else {
                 newElement.innerHTML = `${section.innerHTML} (${rating})`;
+            }
             section.parentNode.replaceChild(newElement, section);
+            section.addEventListener('mouseover', () => {
+                alert("button clicked");
+            });
         }
     }));
 }
+
+// function myAlert(){
+//     alert('hello world');
+// }
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.getElementById('alertButton').addEventListener('click', myAlert);
+// });
+
+// let test = document.querySelector('[id^="MTG_INSTR"]');
+
+// changeColor = (element) => {
+//     console.log("helloooo in popup rn");
+//     element.style.color = "purple";
+// }
+
+// popupListener = (element) => {
+//     console.log("helloooo in popup rn");
+//     // This handler will be executed only once when the cursor
+//     // moves over the unordered list
+//     element.addEventListener("mouseover", function( event ) {
+//         // console.log("hello");
+//         // highlight the mouseenter target
+//         event.target.style.color = "purple";
+//     }, false);
+// }
 
 // sets color based on rating tier
 setTierColor = (rating) => {
@@ -36,7 +67,8 @@ findProfs = (name) => ((name == "To be Announced" || name == "Staff") ? null : n
 
 setup = () => {
     addEval(document);
-    setTimeout(setup, 1000);
+    setTimeout(setup, 2000);
     // funcName("https://www.polyratings.com/list.html")
 }
+console.log("start");
 setup();
