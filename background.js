2c9a079a0b538521f3e74baa20a98883ab1a83df
Matthew Hung <mafewh119@gmail.com>
2022-01-25 13:24:53 -0800

    fixed fetch and parsed HTML response for ratings

--- background.js
+++ background.js
@@ -2,35 +2,24 @@
 // https://developer.chrome.com/docs/extensions/mv3/xhr/
 
 chrome.runtime.onMessage.addListener((request, sender, callback) => {
-    fetch(request.url)
-        .then(response => console.log(response))
-        .then(
-            (response) => {
-                if (response.status != 200) {
-                    console.log('error, status code; ' + response.status);
-                    return;
-                }
-                response.json().then((data) => {
-                    console.log(data);
-                });
+    const init = {
+        method: 'GET'
+    };
+    fetch(request.url, init)
+        .then(response => {
+            if (response.status != 200) {
+                console.log('error, status code ' + response.status);
+                return;
             }
-        )
+            return response.text();
+        })
+        .then((htmlRes) => {
+            // console.log(htmlRes);
+            callback(htmlRes);
+        })
         .catch((err) => {
             console.log('Fetch error: ' + err);
         });
         return true;
-
-    // let xhttp = new XMLHttpRequest();
-    // console.log("here");
-    // // const url = 'polyratings.com/eval/2073/index.html'
-    // xhttp.onload = () => {
-    //     callback(xhttp.responseText);
-    // };
-    // xhttp.onerror = () => {
-        // callback("error");
-    // };
-    // xhttp.open(message.method, message.url, true); // true means do asynch
-    // xhttp.send();
-    // return true;
 });
 
