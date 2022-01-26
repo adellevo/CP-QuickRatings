14d8220d077d67e60343d8e9d8322fcc8f386256
matthewhung09 <mafewh119@gmail.com>
2022-01-25 19:31:44 -0800

    fixed issue with non-alphanumeric characters

--- getProfIds.py
+++ getProfIds.py
@@ -10,21 +10,26 @@ def getHTML(url):
 html = getHTML('https://www.polyratings.com/list.html')
 parsed_html = BeautifulSoup(html, 'html.parser')
 
-# header = ['professor_name', 'professor_id']
+header = ['professor_name', 'professor_id']
 csv_file = open('profIds.csv', 'w', newline='')
 csv_writer = csv.writer(csv_file)
 
 for link in parsed_html.find_all('a', attrs={'href': re.compile("^/eval/")}):
-    professor_name = link.get('data-search')
+    professor_name = link.get('data-search').replace(' ', '')
     # m = re.search(re.search(r'^([^A-Z]*[A-Z]){2}', professor_name)
     # print(m.span()[1])
     professor_id = link.get('href').split('/')[2]
     # print(professor_name + ', ' + professor_id)
     
-    if professor_name.isalpha(): # need to come up with better filter
+    try:
         data = [professor_name, professor_id]
-        csv_writer.writerow(data)
-    else:
+        csv_writer.writerow(data);
+    except:
         print(professor_name)
+    # if professor_name.isalpha(): # need to come up with better filter
+    #     data = [professor_name, professor_id]
+    #     csv_writer.writerow(data)
+    # else:
+    #     print(professor_name)
 
 
