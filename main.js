c6089b9748ce8072b26a10188cd299122dda1e4b
Adelle Vo <adelle.vo@gmail.com>
2022-03-18 20:48:26 -0700

    able to access prof names nowgit add .

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let profs = new Map();
 // sectionArr = array of sections
 // profArr = array of profs for that section
-addEval = () => {
+addEvalSC = () => {
     const sectionArr = document.querySelectorAll('[id*="MTG_INSTR$"]');
     sectionArr.forEach((section) => {
         let profArr = findProfs(section.innerText);
@@ -18,43 +18,44 @@ addEval = () => {
             }
         }
     });
+};
 
-    // ----- schedule builder stuff
+addEvalSB = () => {
+    console.log("addEvalSB");
+    // ------- schedule builder -------
+    var iframe = document.getElementById("ptifrmtgtframe");
+    if (iframe.contentDocument) {
+        // let iframeBody = iframe.contentDocument.body;
+        // // let iName = iframe.contentDocument.body.getElementsByClassName(
+        // //     "cx-MuiTypography-root css-1xnpogb d-flex align-items-center pb-1 pr-1 cx-MuiTypography-body1"
+        // // );
+        let iframeBody = iframe.contentDocument.body;
+        let sectionsList = iframeBody.querySelectorAll('[aria-label="Sections List"]')[0];
+        if (sectionsList != undefined) {
+            // let divList = sectionsList.getElementsByTagName("div");
+            // if (divList != undefined) {
+            //     console.log(divList);
+            // }
+            let sectionInfoContainer = sectionsList.getElementsByClassName(
+                "cx-MuiGrid-root css-11nzenr  cx-MuiGrid-container cx-MuiGrid-item"
+            );
+            if (sectionInfoContainer != undefined) {
+                for (i = 0; i < sectionInfoContainer.length; i++) {
+                    let sectionInfo = sectionInfoContainer.item(i);
+                    if (sectionInfo != undefined) {
+                        let profNameElement = sectionInfo
+                            .getElementsByTagName("dd")
+                            .item(0);
+                        console.log(profNameElement);
+                    }
+                }
+            }
 
-    console.log("helloooo in addEval");
-    // const instructor = document.getElementsByClassName(
-    //     "cx-MuiTypography-root css-1xnpogb d-flex align-items-center pb-1 pr-1 cx-MuiTypography-body1"
-    // )[0];
-    // console.log(`instructor test: ${instructor}`);
-    // const element = document.getElementById("app");
-    // const nodes = document.querySelectorAll("dl.my-0 > dd");
-    // const blah = document.getElementsByClassName(
-    //     "cx-MuiTypography-root css-1xnpogb d-flex align-items-center pb-1 pr-1 cx-MuiTypography-body1"
-    // )[0];
-
-    const blah = document.querySelectorAll('[aria-label*="MTG_INSTR$"]');
-
-    // in a console
-    const div1 = document.getElementById("div1");
-    //=> <div id="div1">Hi Champ!</div>
-
-    const exampleAttr = div1.getAttribute("aria-label");
-    //=> "div1"
-
-    const align = div1.getAttribute("align");
-    //=> null
-
-    // let container = document.querySelectorAll(
-    //     "p-3 d-flex align-items-start flex-wrap > .session > dl > dd"
-    // );
-    // for (let i = 0; i < container.length; i++) {
-    //     let newElement =
-    // }
-    // console.log(blah);
-
-    // document.getElementById("demo").innerHTML = nodes.length;
-    // const blah = document.querySelectorAll('[id*="app"]');
-    // console.log(blah);
+            // console.log(divList);
+        }
+        // console.log(sectionsList.getElementsByTagName("dd").innerText);
+        // console.log(iframeBody.innerHTML);
+    }
 };
 
 initPopup = (profContainer, prof) => {
@@ -67,11 +68,6 @@ initPopup = (profContainer, prof) => {
     const titleDiv = document.createElement("div");
     titleDiv.innerHTML = `<h1>${prof.firstName} ${prof.lastName}</h1><p>Based on ${prof.numEvals} ratings...</p>`;
     popup.appendChild(titleDiv);
-
-    // if (prof.overallRating.length === 3) prof.overallRating = `${prof.overallRating}0`;
-    // if (prof.materialClear.length === 3) prof.materialClear = `${prof.materialClear}0`;
-    // if (prof.studentDifficulties === 3)
-    //     prof.studentDifficulties = `${prof.studentDifficulties}0`;
 
     // create table rows
     const overview = [
@@ -106,17 +102,6 @@ initPopup = (profContainer, prof) => {
 // returns array of professors
 findProfs = (name) =>
     name == "To be Announced" || name == "Staff" ? null : name.split(",");
-
-setup = async () => {
-    // only creates global map of prorfessor names/ids once
-    if (profs.size === 0) {
-        // console.log('getting professors');
-        await getProfessorRatings();
-    }
-    // console.log("helloooo");
-    addEval(document);
-    setTimeout(setup, 1000);
-};
 
 getProfessorInfo = async (profContainer, profArr, i, tempDiv, newElement, section) => {
     // console.log(profArr[i]);
@@ -176,6 +161,17 @@ getProfessorRatings = async () => {
         .catch((err) => {
             console.log("Fetch error: " + err);
         });
+};
+
+setup = async () => {
+    // only creates global map of prorfessor names/ids once
+    if (profs.size === 0) {
+        // console.log('getting professors');
+        await getProfessorRatings();
+    }
+    addEvalSC(document);
+    addEvalSB(document);
+    setTimeout(setup, 1000);
 };
 
 setup();
